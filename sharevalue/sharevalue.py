6b15ab69f35c84bd6b1d33d01be47cd9aad56062
root <root@puspita.(none)>
2013-07-12 19:54:49 +0530

    Re-submission of code

--- sharevalue/sharevalue.py
+++ sharevalue/sharevalue.py
@@ -3,22 +3,18 @@
 import urllib2
 import sys
 
+"""Function is used to get the quoted price of the NASDAQ symbol,which is entered as command line argument."""
 
-def price(sym): #function to get the quoted price of the NASDAQ symbol entered as command line argument.
+def price(sym): 
 
-        quote= urllib2.urlopen('http://download.finance.yahoo.com/d/quotes.csv?s='+sym+'&f=l1') #quote is an instance variable which is storing the quoted price at that instance, of that NASDAQ symbol
-
- #Here we print the quoted price of the NASDAQ symbol entered. 
-        print "This is the  latest quote for the given NASDAQ symbol is "
-        print quote.read() #we use read() as it is an instance variable
-
- #Here is the main function where the argument passed in the commandline is checked to see whether it is a valid input or not.
+    quote= urllib2.urlopen('http://download.finance.yahoo.com/d/quotes.csv?s='+sym+'&f=l1')
+    print "This is the  latest quote for the given NASDAQ symbol is "+quote.read()
+ 
+"""Here is the main function where the argument passed in the commandline is checked to see whether it is a valid input or not and then passed to the function price()."""
 if __name__ == '__main__': 
-        if len(sys.argv) < 2 or len(sys.argv) > 2:
+        if len(sys.argv) !=2:
                 print "Incorrect entry. Enter a NASDAQ symbol"
                 sys.exit(1)
         else:
-#here we pass the NASDAQ symbol as an argument into the price function.
                 price(sys.argv[1])
-                sys.exit(0)
-
+                sys.exit(1)
